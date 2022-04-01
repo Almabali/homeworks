6422eca64022009de977da36950913661d0cdc6a
Almady, Balazs <balazs.almady@gmail.com>
2022-04-01 20:10:06 +0200

    2022-04-01: homeworks

---
+++ homework_01.js
@@ -0,0 +1,18 @@
+/*
+Feladat1:
+Írjatok egy függvényt, ami egy input szám tömbnek megkeresi a legnagyobb elemét (magát a legnagyobb értéket)
+
+Feladat 2
+A legkisebb elem indexét adja vissza
+
+Feladat 3
+Egy függvény ami megszamolja hány a 3-nál kisebb elem van a tömbben
+
+Feladat 4
+Egy függvény, ami egy másik tömböt ad vissza, amiben csak a páros elemek vannak benne.
+
+Feladat 5
+Egy függvényt, ami egy új tömböt ad vissza, amiben az egyes elemeknek a negyzetei vannak.
+
+Plusz az a kettő, amit óra végén mondtam.
+*/
